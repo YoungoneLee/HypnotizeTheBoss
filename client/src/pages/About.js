d6ca2d8a235ffec5ba7d473256848649423f6ed9
Youngone Lee <youngone.lee@accenture.com>
2023-11-27 12:09:32 -0600

    file changed

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,11 +1,11 @@
 import Header from '../components/Header';
-import ListTodos from '../components/ListData';
+import ListGames from '../components/ListData';
 
 export default function About(){
     return(
         <>
             <Header title="About"/>
-            <ListTodos/>
+            <ListGames/>
         </>
     )
 }
---
+++ client/src/pages/GamePage.js
@@ -0,0 +1,5 @@
+//This is where we would put our code for 
+//edit, submit, and deleting a run submission
+//We should get here by clicking onto a specific game 
+//from the game search result page
+//currently this is not routed on app.js -- please do that yourself 
