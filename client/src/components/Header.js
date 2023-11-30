8e79e20ee0cd1cacf72b1f430a19166ab39f40ce
leldridg <leldridg@trinity.edu>
2023-11-29 22:13:42 -0600

    played around some, got a select box to exist, my brain is mush so can't finish rn :(

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,26 +1,67 @@
 import React, {Fragment} from 'react';
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
-import Logo from '../images/waddlesleep.png'; // replace with your logo path
+import { AppBar, Toolbar, Typography, Button, Box, Select, MenuItem, InputLabel, FormControl, TextField } from '@mui/material';
+import Logo from '../images/poliwhirl.jpg'; // replace with your logo path
 import './Header.css'; // import the CSS file
 import InputData from './InputData';
 
 
 export default function Header({title}) {
+
+    const [filter, setFilter] = React.useState('');
+
+    const handleFilterChange = (event) => {
+            setFilter(event.target.value);
+        };
+
+
+    const [searchTerm, setSearchTerm] = React.useState('');
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+    
+
+    const selectStyle = {
+        backgroundColor: '#ffffff',
+        color: '#000000',
+        '&:before': {
+            borderColor: '#000000', // Border color when not focused
+          },
+        '&:after': {
+            borderColor: '#000000', // Border color when focused
+          },
+    };
+
     return (
         <AppBar position="static">
             <Toolbar>
                 <img src={Logo} alt="logo" className="logo" />
+
                 <Typography variant="h6">
                    {title}
                 </Typography>
+                    
+                <Select
+                    labelId="filter-select-label"
+                    id="filter-select"
+                    style={selectStyle}
+                    value={filter || "Runner"} //could use enum instead?
+                    onChange={handleFilterChange}
+                >
+                    <MenuItem value={"Runner"}>Runner</MenuItem> 
+                    <MenuItem value={"Game"}>Game</MenuItem>
+                    <MenuItem value={"Category"}>Category</MenuItem>
+                </Select>
+                
+                
 
-                <Fragment>
+                {/* <Fragment>
                     <InputData/>
-                </Fragment>
+                </Fragment> */}
 
                 <Box sx={{ flexGrow: 1 }} />
 
-                <Button className="customButton">Button</Button>
+                {/* <Button className="customButton">Button</Button> */}
             </Toolbar>
         </AppBar>
     );
