85f899d8079f79c2d90bee9f2fe5b391c9b2df05
Youngone Lee <youngone.lee@accenture.com>
2023-12-04 00:43:48 -0600

    shows even nicer

--- client/src/pages/SimilarRunPage.js
+++ client/src/pages/SimilarRunPage.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import { useLocation } from 'react-router-dom';
 import Header from '../components/Header'
 
@@ -8,10 +12,20 @@ const SimilarRunPage = () => {
     return(
         <>
         <Header title="similar run page"/>
-        <h1> huh</h1>
-        {row && Object.entries(row).map(([key, value], index) => (
-            <h1 key={index}>{key}: {value}</h1>
-        ))}
+        <h1> Chosen Run </h1>
+
+
+        <CssBaseline />
+            <Container fixed  sx={{ bgcolor: '#cfe8fc', height: '200vh' }}>
+            {/* <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} /> */}
+                {row && Object.entries(row).map(([key, value], index) => (
+                <h1 key={index}>{key}: {value}</h1>
+                ))}
+        </Container>
+
+
+
+
         </>
     );
 }
